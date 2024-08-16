19678af7d13b37247fcdb940ffd59383ae4d8b7b
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-08-15 20:39:39 -0700

    Update U2P2.java

--- U2P2.java
+++ U2P2.java
@@ -2,19 +2,35 @@
 initialize the array with 5 numbers and display the array Elements
 in reverse order. */
 import java.util.Scanner;
-class U2P2 {
-
+class U2P2
+{
     public static void main(String args[]) {
-        int array[] = new int[5];
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 5 integer values: ");
-        for (int i = 0; i < 5; i++) {
-            array[i]=scanner.hasNextInt() ? scanner.nextInt() : 0;
-            System.out.println("a [" + i + "]= " + array[i]);
+        Scanner s = new Scanner(System.in);
+        printMessage("Enter element of the array: ");
+        int numberOfElements = getIntegerValue(s);
+        if (numberOfElements == 0) {
+            printMessage("Elements cannot be 0.");
+            return;
         }
-        System.out.println("Array in reverse order: ");
+        int array[] = new int[numberOfElements];
+        printMessage("Enter " + numberOfElements + " integer values: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i]=getIntegerValue(s);
+            printMessage("a [" + i + "]= " + array[i]);
+        }
+
+        printMessage("array in reverse order.");
         for (int i = array.length - 1; i >= 0; i--) {
-            System.out.println("a [" + i + "]= " + array[i]); 
+            printMessage("a [" + i + "]= " + array[i]);
         }
+        s.close();
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static int getIntegerValue(Scanner s) {
+        return s.hasNextInt() ? s.nextInt() : 0;
     }
 }
