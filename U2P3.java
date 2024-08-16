b04f906b6e2d19b3d15358a2905abe3952f7e8dc
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-08-15 20:45:18 -0700

    Update U2P3.java

--- U2P3.java
+++ U2P3.java
@@ -1,26 +1,11 @@
 /* Write a program to find sum of two matrices of 3 x3. */
 import java.util.Scanner;
-
 class U2P3 {
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int[][] mOne = new int[3][3];
-        int[][] mTwo = new int[3][3];
-        System.out.println("Enter values for first 3*3 matrix: ");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("Element [" + i + "][" + j + "]: ");
-                mOne[i][j] = scanner.hasNextInt() ? scanner.nextInt() : 0;
-            }
-        }
-        System.out.println("Enter value for second 3*3 matrix: ");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("Element [" + i + "][" + j + "]: ");
-                mTwo[i][j] = scanner.hasNextInt() ? scanner.nextInt() : 0;
-            }
-        }
+        int[][] mOne = getMatrix(scanner,3, 3);
+        int[][] mTwo = getMatrix(scanner,3, 3);
         scanner.close();
 
         int[][] sum = new int[3][3];
@@ -34,7 +19,18 @@ class U2P3 {
             for (int j = 0; j < 3; j++) {
                 System.out.print(sum[i][j] + " ");
             }
-            System.out.println();
+            System.out.println( );
         }
+    }
+
+    public static int[][] getMatrix(Scanner scanner, int rows, int columns) {
+        int matrix[][] = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.println("Element [" + i + "][" + j + "]: ");
+                matrix[i][j] = scanner.hasNextInt() ? scanner.nextInt() : 0;
+            }
+        }
+        return matrix;
     }
 }
