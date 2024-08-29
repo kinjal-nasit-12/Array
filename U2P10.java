8335edb2e05d2f653a7c57589c856b851c00c138
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-08-29 09:32:45 -0700

    Update U2P10.java

--- U2P10.java
+++ U2P10.java
@@ -4,53 +4,43 @@ hierarchy. Declare one instance variable of type string that indicates the color
 a vegetable. Create and display instances of these object. Override the toString()
 method of object to return a string with the name of the vegetable and its color. */
 abstract class Vegetable {
-
     String color;
-
     abstract public String toString();
 }
-
 class Cabbage extends Vegetable {
-
     Cabbage(String s) {
         color = s;
     }
-
     public String toString() {
         return ("Cabbage color: " + color);
     }
 }
-
 class Carrot extends Vegetable {
-
     Carrot(String s) {
         color = s;
     }
-
     public String toString() {
         return ("Carrot color: " + color);
     }
 }
-
 class Potato extends Vegetable {
-
     Potato(String s) {
         color = s;
     }
-
     public String toString() {
         return ("Potato color: " + color);
     }
 }
-
 class U2P10 {
-
-    public static void main(String ags[]) {
+     public static void main(String ags[]) {
         Cabbage cabbage = new Cabbage("Green");
         Carrot carrot = new Carrot("Red");
         Potato potato = new Potato("Yellow");
-        System.out.println(cabbage.toString());
-        System.out.println(carrot.toString());
-        System.out.println(potato.toString());
+        printMessage(cabbage.toString());
+        printMessage(carrot.toString());
+        printMessage(potato.toString());
+    }
+     public static void printMessage(String message) {
+        System.out.println(message);
     }
 }
