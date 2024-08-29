9562f2315bd236fe98f6237ecc2872c393decfb6
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-08-29 09:28:15 -0700

    Update U2P9.java

--- U2P9.java
+++ U2P9.java
@@ -1,44 +1,35 @@
 /* Create class calculation with an abstract method area( ). Create Rectangle and
 Triangle subclasses of calculation and find area of rectangle and triangle. */
 import java.util.Scanner;
-
 abstract class Calculation {
-
-    abstract void area(int a, int b);
+    abstract void area(float a, float b);
 }
-
 class Rectangle extends Calculation {
-
-    public void area(int a, int b) {
-        int c = a * b;
+    public void area(float a, float b) {
+        float c = a * b;
         System.out.println("Area of rectangle is : " + c);
     }
 }
-
 class Triangle extends Calculation {
-
-    public void area(int a, int b) {
-        int c = (a + b) / 2;
+     public void area(float a, float b) {
+        float c = (a + b) / 2;
         System.out.println("Area of Triangle is : " + c);
     }
 }
-
 class U2P9 {
-
     public static void main(String args[]) {
         Rectangle rectangle = new Rectangle();
         Triangle triangle = new Triangle();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter length : ");
-        int a = scanner.hasNextInt() ? scanner.nextInt() : 0;
-        if(a==0)
-        {
-            System.out.println("Invalid: Enter input in integer value.");
-            return ;
-        }
-        System.out.println("Enter width: ");
-        int b = scanner.hasNextInt() ? scanner.nextInt() : 0;
+        printMessage("Enter length : ");
+        float a = scanner.hasNextFloat() ? scanner.nextFloat() : 0;
+        printMessage("Enter width: ");
+        float b = scanner.hasNextFloat() ? scanner.nextFloat() : 0;
+        scanner.close();
         rectangle.area(a, b);
         triangle.area(a, b);
+    }
+    public static void printMessage(String message) {
+        System.out.println(message);
     }
 }
