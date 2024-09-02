4b5234d5e1c456c479cbbbaeaf7b3d70e0179f10
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-02 08:26:50 -0700

    Update U2P4.java

--- U2P4.java
+++ U2P4.java
@@ -1,3 +1,4 @@
+
 /* Write program to create an array of company name and another array of price
 quoted by the company. Fetch the company name who has quoted the lowest
 amount. */
@@ -10,32 +11,30 @@ class U2P4 {
         printMessage("Please enter number of companies that you want to enter: ");
         int n = scanner.hasNextInt() ? scanner.nextInt() : 0;
 
-        if(n<=0)
-        {
-            printMessage("You must enter a number greater than 0 fot the number of companies.");
-        }
-        else{
-        int a[] = new int[n];
-        String string[] = new String[n];
-        for (int i = 0; i < n; i++) {
-            printMessage("Enter company name: ");
-            string[i] = scanner.next();
-            printMessage("Enter quotation: ");
-            a[i] = scanner.hasNextInt() ? scanner.nextInt() : 0;
+        if (n <= 0) {
+            printMessage("You must enter a number greater than 0 for the number of companies.");
+        } else {
+            int a[] = new int[n];
+            String string[] = new String[n];
+            for (int i = 0; i < n; i++) {
+                printMessage("Enter company name: ");
+                string[i] = scanner.next();
+                printMessage("Enter quotation: ");
+                a[i] = scanner.hasNextInt() ? scanner.nextInt() : 0;
+            }
+            int min = a[0];
+            int j = 0;
+            for (int i = 1; i < a.length; i++) {
+                if (a[i] < min) {
+                    min = a[i];
+                    j = i;
+                }
+            }
+            printMessage("Name of the company : " + string[j] + " with lowest Quotation: " + a[j]);
         }
         scanner.close();
-        int min = a[0];
-        int j = 0;
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] < min) {
-                min = a[i];
-                j = i;
-            }
-        }
-        printMessage("Name of the company : " + string[j] + " with lowest Quotation: " + a[j]);
     }
-    scanner.close();
-}
+
     public static void printMessage(String message) {
         System.out.println(message);
     }
