d0fe46f990ef8ea18b15b6ae6de326a0754dbcc1
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-09-02 08:36:25 -0700

    Update U2P5.java

--- U2P5.java
+++ U2P5.java
@@ -3,21 +3,17 @@ class called Sum, in which the method Process finds the sum of two numbers and
 returns an int value. Write another class called Average, in which the Process
 method finds the average of the two numbers and returns an int. */
 import java.util.Scanner;
-
 interface Numbers {
-
     float Process(int x, int y);
 }
 
 class Sum implements Numbers {
-
     public float Process(int x, int y) {
         return x + y;
     }
 }
 
 class Average implements Numbers {
-
     public float Process(int x, int y) {
         return (float) (x + y) / 2;
     }
@@ -29,17 +25,16 @@ class U2P5 {
         Scanner scanner = new Scanner(System.in);
         Sum sum = new Sum();
         Average avg = new Average();
-
-        System.out.println("Enter integer value 1: ");
+        printMessage("Enter integer value 1: ");
         int x = scanner.hasNextInt() ? scanner.nextInt() : 0;
-        if(x==0)
-        {
-            System.out.println("Invalid: Enter integer value.");
-            return ;
-        }
-        System.out.println("Enter integer value 2: ");
+        printMessage("Enter integer value 2: ");
         int y = scanner.hasNextInt() ? scanner.nextInt() : 0;
-        System.out.println("Sum of two numbers: " + sum.Process(x, y));
-        System.out.println("Average is: " + avg.Process(x, y));
+        scanner.close();
+        printMessage("Sum of two numbers: " + sum.Process(x, y));
+        printMessage("Average is: " + avg.Process(x, y));
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
     }
 }
